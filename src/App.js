7a3288be3b62a4d2288d8d1c43bce90a9225cbda
Ali Martin <alistairmartin@live.com>
2018-08-31 13:59:00 +0100

    Added in bootstrap css

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <img src={require('./IMG_1152.jpg')} alt="home page" className="App-image"></img>
+      <div className="App container">
+        <Header className="col-xs-12 col-md-7"/>
+        <img src={require('./IMG_1152.jpg')} alt="home page" className="App-image col-xs-12 col-md-5"></img>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
